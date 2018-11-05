0e7f2cbe54020c076c6d9d1997f0174cb183d097
Javier Mallén <javiermallen@yahoo.es>
2018-11-05 21:55:04 +0100

    Ejercicio Finalizado

--- planetas/main.js
+++ planetas/main.js
@@ -1,1 +1,49 @@
+Vue.component( 'mostrarplanetas',{
+    props: [
+        'planeta',
+        'visitado'
+    ],
+    template: "#planeta"
 
+
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        planetas: [
+            {
+                nombre: 'Mercurio',
+                visitas: 0
+            },
+            {
+                nombre: 'Venús',
+                visitas: 0
+            },
+            {
+                nombre: 'Tierra',
+                visitas: 0
+            },
+            {
+                nombre: 'Marte',
+                visitas: 1
+            },
+            {
+                nombre: 'Jupiter',
+                visitas: 3
+            },
+            {
+                nombre: 'Saturno',
+                visitas: 0
+            },
+            {
+                nombre: 'Urano',
+                visitas: 0
+            },
+            {
+                nombre: 'Plutón',
+                visitas: 0
+            }
+        ]
+    }
+})
