f5477e34dd34856a055a16fb7b92aa2687c9f1d3
Javier Mallén <javiermallen@yahoo.es>
2018-11-06 22:39:39 +0100

    Ejercicio acabado

--- cambio color/main.js
+++ cambio color/main.js
@@ -1,1 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+        color: {
+            value:'#c2c2c2'
+        }
+    },
+    methods: {
+        colorSeleccionado (){
+            console.log( color.value )
+        }
+    }
 
+})
