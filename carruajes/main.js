cc5d33f46f3f73acdb024a909fc12dd4b6ae2aa6
Javier Mallén <javiermallen@yahoo.es>
2018-11-06 21:53:35 +0100

    Primer acercamiento
    Falta la lógica del botón

--- carruajes/main.js
+++ carruajes/main.js
@@ -1,1 +1,43 @@
+Vue.component('carruaje',{
+    template: '#carruajes',
+    props: ['item']
 
+
+
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        carruaje: [
+            {
+                id: 1,
+                nombre: 'Olimpus',
+                caballos: 3
+            },
+            {
+                id: 2,
+                nombre: 'Nexus',
+                caballos: 4
+            },           
+            {
+                id: 3,
+                nombre: 'Dimitrus',
+                caballos: 2
+            },
+            {
+                id: 4,
+                nombre: 'Zeus',
+                caballos: 1
+            },
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+
+
+})
